609950143936c359f5ab715d8c6e55071b69881f
Noir97 <wangnengtututu@gmail.com>
2019-11-03 01:57:14 -0800

    delete test file

--- lab2/test.py
+++
@@ -1,6 +0,0 @@
-import math
-print(math.atan2(math.sqrt(3),1)/math.pi)
-print(math.atan2(math.sqrt(3),-1)/math.pi)
-print(math.atan2(-math.sqrt(3),-1)/math.pi)
-print(math.atan2(-math.sqrt(3),1)/math.pi)
-print((-math.pi)%(2*math.pi))
