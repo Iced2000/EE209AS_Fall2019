d471c04fa0c54e44c2ce8a538854f27ea1a4a99c
Noir97 <wangnengtututu@gmail.com>
2019-11-03 13:24:28 -0800

    update

--- lab2/env.py
+++ lab2/env.py
@@ -11,13 +11,16 @@ class Robot():
         self.x = 0              # center x,y & orientation
         self.y = 0
         self.orientation = 0
-        self.maxSpeed = 2 * math.pi * self.r       # wheel speed 1 revolution/s = 60 RPM
+        self.omegaMax = 2 * math.pi       # max wheel speed 1 revolution/s = 60 RPM = 2pi rad/s
     
     def setx(self, x):
         self.x = x
     
     def sety(self, y):
         self.y = y
+
+    def setOrientation(self, o):
+        self.orientation = o
     
     def computeMove(self, x, y, theta, k1, k2, k3, t):
         dx = x - self.x
@@ -45,29 +48,41 @@ class Robot():
 
 class Environment():
 
-    def __init__(self, W, L, obs, target):
+    def __init__(self, W, L):
         self.W = W
         self.L = L
         self.robot = Robot()
-        self.obs = obs                                                  # list of obstacles in the form of (x, y, w, l)
+        self.obs = []                                           # list of obstacles in the form of (x, y, w, l)
         
-        self.target = target
-        while self.colDetect(self.target[0], self.target[1]):
-            print('target has potential risk of collision, reset to random !')
-            self.target = (self.W * random.random(), self.L * random.random())                                          # e.g. [3*W/4, 3*L/4]
+        # self.target = target
+        self.target = (self.W * random.random(), self.L * random.random(), 2 * math.pi * random.random() - math.pi)
+        while self.colDetect(self.target):
+            # print('target has potential risk of collision, reset to random !')
+            self.target = (self.W * random.random(), self.L * random.random(), 2 * math.pi * random.random() - math.pi)                                          # e.g. [3*W/4, 3*L/4]
         
-        self.init = (self.W * random.random(), self.L * random.random())
-        while self.colDetect(self.init[0], self.init[1]):
-            self.init = (self.W * random.random(), self.L * random.random())
+        self.init = (self.W * random.random(), self.L * random.random(), 2 * math.pi * random.random() - math.pi)
+        while self.colDetect(self.init):
+            self.init = (self.W * random.random(), self.L * random.random(), 2 * math.pi * random.random() - math.pi)
         
         self.robot.setx(self.init[0])
         self.robot.sety(self.init[1])
+        self.robot.setOrientation(self.init[2])
 
-
-    def colDetect(self, x, y):
+    def colDetect(self, state):
+        # make sure four corners of the robot are safe.
+        corners = np.zeros((4,2))
+        theta1 = math.atan2(self.robot.w/2, 25)
+        theta2 = math.atan2(self.robot.w/2, self.robot.l)
+        rou1 = math.sqrt((self.robot.w/2)**2 + 25**2)
+        rou2 = math.sqrt((self.robot.w/2)**2 + self.robot.l**2)
+        corners[0]= [state[0] + rou1*math.cos(state[2]-theta1), state[1] + rou1*math.sin(state[2]-theta1)]
+        corners[1]= [state[0] + rou1*math.cos(state[2]+theta1), state[1] + rou1*math.sin(state[2]+theta1)]
+        corners[2]= [state[0] + rou2*math.cos(state[2]-theta2), state[1] + rou2*math.sin(state[2]-theta2)]
+        corners[3]= [state[0] + rou2*math.cos(state[2]+theta2), state[1] + rou2*math.sin(state[2]+theta2)]
         for o in self.obs:
-            if abs(x - o[0]) < o[2]/2 and abs(y - o[1]) < o[3]/2 :
-                return True
+            for i in range(4):
+                if abs(corners[i][0] - o[0]) < o[2]/2 and abs(corners[i][1] - o[1]) < o[3]/2 :
+                    return True
         return False
 
     def setObs(self, obs):
@@ -75,9 +90,15 @@ class Environment():
 
     def setTarget(self, target):
         self.target = target
-        while self.colDetect(self.target[0], self.target[1]):
-            print('target has potential risk of collision, reset to random !')
-            self.target = (self.W * random.random(), self.L * random.random())
+        while self.colDetect(self.target):
+            print('target state has potential risk of collision, reset to random !')
+            self.target = (self.W * random.random(), self.L * random.random(), 2 * math.pi * random.random() - math.pi)
+
+    def setInit(self, init):
+        self.init = init
+        while self.colDetect(self.init):
+            print('initial state has potential risk of collision, reset to random !')
+            self.init = (self.W * random.random(), self.L * random.random(), 2 * math.pi * random.random() - math.pi)
 
     def genTraj(self):                                                  # should include RRT here and get a traj
         traj = []
@@ -86,7 +107,7 @@ class Environment():
 obs = []
 obs.append((5,5,1,1))
 obs.append((2,2,1,1))
-target = (8, 8)
-e = Environment(10, 10, obs, target)
-dr, angle, forward = e.robot.computeMove(e.target[0], e.target[1])
+target = (8, 8, math.pi)
+e = Environment(10, 10)
+controlInputs = e.robot.computeMove(e.target[0], e.target[1], e.target[2], 0.3, 1.5, -0.3, 0.1)
 print('1')
