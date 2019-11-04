b17e3524c0403ef316179a1f17203e10dc34eb6a
Noir97 <wangnengtututu@gmail.com>
2019-11-03 16:50:09 -0800

    trajgen and obsplot added

--- lab2/env.py
+++ lab2/env.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import random
+import matplotlib.pyplot as plt
 
 class Robot():
 
@@ -21,6 +22,11 @@ class Robot():
 
     def setOrientation(self, o):
         self.orientation = o
+    
+    def setInit(self, init):
+        self.x = init[0]
+        self.y = init[1]
+        self.orientation = init[2]
     
     def computeMove(self, target, k1, k2, k3, t):
         dx = target[0] - self.x
@@ -44,7 +50,7 @@ class Robot():
         self.x = self.x + v * math.cos(self.orientation) * t
         self.y = self.y + v * math.sin(self.orientation) * t
         
-        return (omegaLeft, omegaRight)
+        return np.array([[self.x, self.y, self.orientation]]), (omegaLeft, omegaRight)
 
 class Environment():
 
@@ -53,7 +59,7 @@ class Environment():
         self.L = L
         self.robot = Robot()
         self.obs = []                                           # list of obstacles in the form of (x, y, w, l)
-        
+        self.goalRegion = []
         # self.target = target
         self.target = (self.W * random.random(), self.L * random.random(), 2 * math.pi * random.random() - math.pi)
         while self.colDetect(self.target):
@@ -64,9 +70,7 @@ class Environment():
         while self.colDetect(self.init):
             self.init = (self.W * random.random(), self.L * random.random(), 2 * math.pi * random.random() - math.pi)
         
-        self.robot.setx(self.init[0])
-        self.robot.sety(self.init[1])
-        self.robot.setOrientation(self.init[2])
+        self.robot.setInit(self.init)
 
     def colDetect(self, state):
         # make sure four corners of the robot are safe.
@@ -81,12 +85,15 @@ class Environment():
         corners[3]= [state[0] + rou2*math.cos(state[2]+theta2), state[1] + rou2*math.sin(state[2]+theta2)]
         for o in self.obs:
             for i in range(4):
-                if abs(corners[i][0] - o[0]) < o[2]/2 and abs(corners[i][1] - o[1]) < o[3]/2 :
+                if abs(corners[i][0] - o[0] - o[2]/2) < o[2]/2 and abs(corners[i][1] - o[1] - o[3]/2) < o[3]/2 :
                     return True
         return False
 
     def setObs(self, obs):
         self.obs = obs
+
+    def setGoalRegion(self, gr):
+        self.goalRegion = gr
 
     def setTarget(self, target):
         self.target = target
@@ -99,18 +106,47 @@ class Environment():
         while self.colDetect(self.init):
             print('initial state has potential risk of collision, reset to random !')
             self.init = (self.W * random.random(), self.L * random.random(), 2 * math.pi * random.random() - math.pi)
-        self.robot.setx(self.init[0])
-        self.robot.sety(self.init[1])
-        self.robot.setOrientation(self.init[2])
+        self.robot.setInit(self.init)
 
-    def genTraj(self):                                                 
-        traj = []
-        return traj
+    def trajGen(self, init, target, tf, dt, k1, k2, k3):
+        t = 0
+        controlInputs = []
+        traj = np.reshape(init, [1, 3])
+        self.robot.setInit(init)
+        while t < tf :
+            state, input = self.robot.computeMove(target, k1, k2, k3, dt)    # k1,k2,k3 needs to be modified later!!!
+            controlInputs.append(input)
+            traj = np.concatenate((traj, state), axis=0)
+            t = t + dt
+
+        plt.figure()
+        plt.arrow(init[0], init[1], np.cos(init[2]), np.sin(init[2]), color='r', width=1)
+        plt.arrow(target[0], target[1], np.cos(target[2]), np.sin(target[2]), color='g', width=1)
+        plt.plot(traj[:,0], traj[:,1])
+        plt.ylim([0, self.L])
+        plt.xlim([0, self.W])
+        plt.show()
+
+        return controlInputs, traj
+
+    def plotObs(self):
+        fig = plt.figure()
+        ax = fig.gca()
+        for ob in self.obs:
+            obstacle = plt.Rectangle(ob[0:2], ob[2], ob[3], color = 'k')
+            ax.add_patch(obstacle)
+        plt.xlim([0, self.W])
+        plt.ylim([0, self.L])
+        plt.show()
+
+    def RRT(self):                                                 
+        nodes = []
+        return nodes
 
 # obs = []
 # obs.append((5,5,1,1))
 # obs.append((2,2,1,1))
 # target = (8, 8, math.pi)
 # e = Environment(10, 10)
-# controlInputs = e.robot.computeMove(e.target, 0.3, 1.5, -0.3, 0.1)
+# state, controlInputs = e.robot.computeMove(e.target, 0.3, 1.5, -0.3, 0.1)
 # print('1')
--- lab2/solution.py
+++ lab2/solution.py
@@ -1,17 +1,28 @@
 import env
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 e = env.Environment(100, 100)
 e.setTarget((80, 80, 0))
-e.setInit((10, 10, math.pi))
+e.setInit((10, 10, math.pi/2))
 
-t = 0
-dt = 0.1
-controlInputs = []
-while t < 1 :
-    controlInputs.append(e.robot.computeMove(e.target, 0.3, 1.5, -0.3, dt))
-    t = t + dt
+# e.trajGen(e.init, e.target, 1, 0.05, 0.3, 1.5, -0.3)        # k1,k2,k3 need to be modified
+
+obs = [
+    (0, 0, 10, 10),
+    (50, 0, 20, 20),
+    (70, 80, 20, 20),
+    (0, 30, 30, 10),
+    (0, 70, 20, 10),
+    (80, 50, 20, 10),
+    ]
+e.setObs(obs)
+
+# e.plotObs()
+goalRegion = [(50, 50, 10, 10)]
+
+
 
 
 
